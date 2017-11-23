27f1f7706ffad82266c06012e5b07fcfa3ed3490
Cory LePage <cory@lepagegroup.ca>
2017-11-22 16:00:11 -0800

    Initial commit

--- ExtendBeamsToBeam/Program.cs
+++ ExtendBeamsToBeam/Program.cs
@@ -1,15 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
+using Tekla.Structures.Model.Operations;
 
 namespace ExtendBeamsToBeam
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
+			try
+			{
+				ExtendBeamsToBeam.Run();
+			}
+			catch (Exception ex)
+			{
+				Trace.WriteLine(ex.InnerException + ex.Message + ex.StackTrace);
+			}
+		}
+
+		public static class ExtendBeamsToBeam
+		{
+			public static void Run()
+			{
+				var model = new Model();
+				var picker = new Picker();
+
+				try
+				{
+					Operation.DisplayPrompt("Select beams to extend to beam");
+
+					var selected = picker.PickObjects(Picker.PickObjectsEnum.PICK_N_PARTS);
+					if (selected.GetSize() < 1) return;
+
+					Operation.DisplayPrompt("Select beam");
+
+					var pickedBeam = picker.PickObject(Picker.PickObjectEnum.PICK_ONE_PART) as Beam;
+					if (pickedBeam == null) return;
+
+					var coordSystem = pickedBeam.GetCoordinateSystem();
+					var intersectionPlane = new GeometricPlane(coordSystem.Origin, coordSystem.AxisX,
+						new Vector(0, 0, 1));
+
+					Operation.DisplayPrompt("Beams are being extended/trimmed to the beam");
+
+					while (selected.MoveNext())
+					{
+						var beam = selected.Current as Beam;
+						if (beam == null) continue;
+
+						var beamLine = new Line(beam.StartPoint, beam.EndPoint);
+						var intersectPoint = Intersection.LineToPlane(beamLine, intersectionPlane);
+						if(intersectPoint == null) continue;
+
+						var startDist = Distance.PointToPoint(intersectPoint, beam.StartPoint);
+						var endDist = Distance.PointToPoint(intersectPoint, beam.EndPoint);
+
+						if (startDist <= endDist)
+							beam.StartPoint = intersectPoint;
+						else
+							beam.EndPoint = intersectPoint;
+
+						beam.Modify();
+					}
+
+					Operation.DisplayPrompt("Beams have been extended/trimmed to the beam");
+				}
+				catch (Exception ex)
+				{
+					
+				}
+
+				model.CommitChanges();
+			}
 		}
 	}
 }
